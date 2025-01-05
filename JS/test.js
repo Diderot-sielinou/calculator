43c6147861379e3db4730b1fc08f976ed6a57241
diderot-sielinou <diderotsielinou@gmail.com>
2025-01-05 02:30:49 +0100

    add linter standar to resolve linters error

--- JS/test.js
+++ JS/test.js
@@ -1,25 +1,31 @@
-let input = document.getElementById("inputbox");
-let buttons = document.querySelectorAll("button");
-let string = "";
-let arr = Array.from(buttons);
-console.log(arr);
-arr.forEach((button) => {
-  console.log(button);
-  button.addEventListener("click", (e) => {
-    console.log(e);
+const input = document.getElementById('inputbox')
+const buttons = document.querySelectorAll('button')
+let string = ''
+const arr = Array.from(buttons)
+console.log(arr)
+arr.forEach(button => {
+  console.log(button)
+  button.addEventListener('click', (e) => {
+    // console.log(e.target.innerHTML)
 
-    if (e.target.innerHTML == "=") {
-      string = eval(string);
-      input.value = string;
-    } else if (e.target.innerHTML == "AC") {
-      string = "";
-      input.value = string;
-    } else if (e.target.innerHTML == "DEL") {
-      string = string.substring(0, string.length - 1);
-      input.value = string;
+    if (e.target.innerHTML === '=') {
+      string = eval(string) // eslint-disable-line
+      input.value = string
+    } else if (e.target.innerHTML === '%') {
+      console.log(typeof parseFloat(string))
+      const percentage = parseFloat(string) / 100
+      console.log(percentage)
+      input.value = percentage
+      string = percentage
+    } else if (e.target.innerHTML === 'AC') {
+      string = ''
+      input.value = string
+    } else if (e.target.innerHTML === 'DEL') {
+      string = string.substring(0, string.length - 1)
+      input.value = string
     } else {
-      string += e.target.innerHTML;
-      input.value = string;
+      string += e.target.innerHTML
+      input.value = string
     }
-  });
-});
+  })
+})
